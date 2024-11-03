2ec6c385bbc88dfc0d4878816483df94936f42d4
Lucas pereira <lucasgunner3@gmail.com>
2024-11-03 11:05:38 -0300

    feat: resetando projeto com maven

---
+++ src/main/java/com/descomplica/frameblog/FrameblogApplication.java
@@ -0,0 +1,13 @@
+package com.descomplica.frameblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameblogApplication.class, args);
+	}
+
+}
